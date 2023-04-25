1f462ae574ea1f340e57dd2004de822e58a51275
dlgnlwo3 <dlgnlwo3@naver.com>
2023-04-25 17:50:20 +0900

    로그인 구현

--- process/sabangnet_regist_upload_process.py
+++ process/sabangnet_regist_upload_process.py
@@ -38,7 +38,7 @@ from common.google_sheet import get_gs_data
 class SabangnetRegistUploadProcess:
     def __init__(self):
         self.default_wait = 10
-        self.driver: webdriver.Chrome = get_chrome_driver_new(is_headless=True, is_secret=False)
+        self.driver: webdriver.Chrome = get_chrome_driver_new(is_headless=False, is_secret=True)
         self.driver.implicitly_wait(self.default_wait)
         self.driver.maximize_window()
 
@@ -48,9 +48,80 @@ class SabangnetRegistUploadProcess:
     def setLogger(self, log_msg):
         self.log_msg = log_msg
 
+    def sabangnet_login(self):
+        driver = self.driver
+        driver.get(f"https://www.sabangnet.co.kr/")
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//a[contains(@href, "index.html")]'))
+        )
+        time.sleep(0.2)
+
+        login_id = self.guiDto.sabangnet_id
+        login_pw = self.guiDto.sabangnet_pw
+
+        # 로그인 시도
+        try:
+            driver.implicitly_wait(2)
+
+            input_id = driver.find_element(By.XPATH, '//input[@id="txtID"]')
+            input_id.clear()
+            input_id.send_keys(login_id)
+            time.sleep(0.2)
+
+            input_pwd = driver.find_element(By.XPATH, '//input[@id="txtPWD"]')
+            input_pwd.clear()
+            input_pwd.send_keys(login_pw)
+            time.sleep(0.2)
+
+            login_button = driver.find_element(
+                By.XPATH, '//button[contains(@onclick, "chk") and contains(text(), "로그인")]'
+            )
+            login_button.click()
+
+            # 사방넷 접속 버튼 대기
+            WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "사방넷 접속")]'))
+            )
+            time.sleep(0.2)
+
+            driver.find_element(By.XPATH, '//button[contains(text(), "사방넷 접속")]').click()
+
+            # 대시보드 로딩 대기
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, '//a[contains(@href, "dashboard")]'))
+            )
+            time.sleep(0.2)
+
+        except Exception as e:
+            print(e)
+            raise Exception(f"사방넷 로그인 실패")
+
+        finally:
+            driver.implicitly_wait(self.default_wait)
+
     # 전체작업 시작
     def work_start(self):
         print(f"process: work_start")
+
+        try:
+            for target_date in self.guiDto.target_date_list:
+                self.target_date = target_date
+
+                self.sabangnet_login()
+
+                try:
+                    print(f"[{self.target_date}] 작업 시작")
+
+                except Exception as e:
+                    print(e)
+                    continue
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            self.driver.close()
+            time.sleep(0.2)
 
 
 if __name__ == "__main__":
